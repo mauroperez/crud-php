e606a1b062422d07c1fc06c9ed9124a960cd47a8
Lucas Pérez <lucas.perez@ideais.com.br>
2014-05-29 21:32:04 -0300

    alterando a validacao js

--- produto-formulario.php
+++ produto-formulario.php
@@ -3,23 +3,20 @@
 
 <head>
 <script type="text/javascript">
-function validaCampo()
-{
-if(document.cadform.nome.value=="")
-{
-alert("O Campo nome é obrigatório!");
-return false;
-}
-else
-return true;
-}
+function validaCampo() {
+	if(document.cadform.nome.value=="" || document.cadform.preco.value=="") {
+		alert("O Campo nome é obrigatório!");
+		return false;
+	}
+	else 
+		return true;
 </script>
 </head>
 
 <body>
         <h1>Formulário de Cadastro Teste</h1>
         <form id="cadform" name="cadform" method="get" action="adiciona-produtos.php" onsubmit="return validaCampo();">
-                    
+
             Nome: <input type="text" name="nome" id="nome"/><br/>
                   
             Preço:<input type="number" name="preco" /><br/>
